aeecd5f7b7ba38241b3d84375f74954484ad5ca0
Ranaji91969 <159106117+Ranaji91969@users.noreply.github.com>
2024-02-22 09:25:07 +0530

    Add files via upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,40 @@
+import React from "react"
+import { homeData } from "../../dummyData"
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className='container'>
+          <div className='box'>
+            <ul className='flex'>
+              <li>Terms of Use</li>
+              <li>Privacy-Policy</li>
+              
+              <li>Watch List</li>
+            </ul>
+            <p>© 2024 Deftsoft Limited. All Rights Reserved. </p>
+          </div>
+          <div className='box'>
+            <h3>Follow Us</h3>
+            <i className='fab fa-facebook-f'></i>
+            <i className='fab fa-twitter'></i>
+            <i className='fab fa-github'></i>
+            <i className='fab fa-instagram'></i>
+          </div>
+          <div className='box'>
+            <h3>Deftsoft Fun Player</h3>
+            <div className='img flexSB'>
+              
+              <img src='https://img.icons8.com/fluency/48/000000/google-play.png' />
+              <span>Google Play Store</span>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
